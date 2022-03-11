fd7c967d4505803c1e47e51477551fc8afb5568c
Jesús Enrique Rascón <blograso@gmail.com>
2022-03-11 10:46:51 +0100

    charts work fine within manim!

--- PNGvsQOI/png_qoi.py
+++ PNGvsQOI/png_qoi.py
@@ -3176,6 +3176,20 @@ class Filtering(MovingCameraScene):
 
 class TestBarChart(Scene):
     def construct(self):
-        barchart = SVGMobject("sample_bar_exp.svg").scale(3)
+        encode_ms = SVGMobject("encode_ms.svg").scale(3)
+        decode_ms = SVGMobject("decode_ms.svg").scale(3)
+        comp_rate = SVGMobject("comp_rate.svg").scale(3)
 
-        self.play(Write(barchart))
+        self.play(Write(encode_ms))
+
+        self.wait()
+
+        self.play(FadeOut(encode_ms))
+        self.play(Write(decode_ms))
+
+        self.wait()
+
+        self.play(FadeOut(decode_ms))
+        self.play(Write(comp_rate))
+
+        self.wait()
