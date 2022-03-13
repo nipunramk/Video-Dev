7a4540d46c63ac9c7ca0c64140320b326b35cb87
Jesús Enrique Rascón <blograso@gmail.com>
2022-03-13 19:47:59 +0100

    remove barcharts code from png_qoi file

--- PNGvsQOI/png_qoi.py
+++ PNGvsQOI/png_qoi.py
@@ -3172,24 +3172,3 @@ class Filtering(MovingCameraScene):
         return self.camera.frame.animate.set_width(mobject.width * buff).move_to(
             mobject
         )
-
-
-class TestBarChart(Scene):
-    def construct(self):
-        encode_ms = SVGMobject("encode_ms.svg").scale(3)
-        decode_ms = SVGMobject("decode_ms.svg").scale(3)
-        comp_rate = SVGMobject("comp_rate.svg").scale(3)
-
-        self.play(Write(encode_ms))
-
-        self.wait()
-
-        self.play(FadeOut(encode_ms))
-        self.play(Write(decode_ms))
-
-        self.wait()
-
-        self.play(FadeOut(decode_ms))
-        self.play(Write(comp_rate))
-
-        self.wait()
