2ebf148addf8b0f3e19c8ec2e3200daeebe2d31d
Jesús Enrique Rascón <blograso@gmail.com>
2022-04-30 19:29:32 +0200

    remove boilerplate code in TransitionMatrix scene

--- PageRank/jesus_animations.py
+++ PageRank/jesus_animations.py
@@ -13,50 +13,7 @@ import numpy as np
 
 class TransitionMatrix(MovingCameraScene):
     def construct(self):
-        markov_ch = MarkovChain(
-            5,
-            edges=[
-                (2, 0),
-                (3, 0),
-                (4, 0),
-                (2, 3),
-                (0, 3),
-                (4, 1),
-                (2, 1),
-                (0, 2),
-                (3, 4),
-            ],
-        )
-
-        markov_ch_mob = MarkovChainGraph(
-            markov_ch,
-            curved_edge_config={"radius": 2},
-            layout_scale=2.6,
-        )
-
-        self.play(Write(markov_ch_mob))
-
-        self.wait()
-
-        self.play(self.focus_on(markov_ch_mob, buff=2.8).shift(LEFT * 2.5))
-        self.play(
-            Indicate(markov_ch_mob.vertices[0]),
-        )
-
-        self.play(
-            markov_ch_mob.vertices[1].animate.set_opacity(0.3),
-            markov_ch_mob.edges[(2, 1)].animate.set_opacity(0.3),
-            markov_ch_mob.edges[(4, 1)].animate.set_opacity(0.3),
-            markov_ch_mob.edges[(3, 4)].animate.set_opacity(0.3),
-            markov_ch_mob.edges[(2, 3)].animate.set_opacity(0.3),
-            markov_ch_mob.edges[(0, 3)].animate.set_opacity(0.3),
-            markov_ch_mob.edges[(0, 2)].animate.set_opacity(0.3),
-        )
-
-    def focus_on(self, mobject, buff=2):
-        return self.camera.frame.animate.set_width(mobject.width * buff).move_to(
-            mobject
-        )
+        pass
 
 
 class DefineStationaryDist(Scene):
