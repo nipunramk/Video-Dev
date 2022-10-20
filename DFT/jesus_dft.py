90c22a8918185d0a31382ed1dd0ead8847786de4
Jesús Enrique Rascón <blograso@gmail.com>
2022-10-20 14:17:41 +0200

    sampling dots!

--- DFT/jesus_dft.py
+++ DFT/jesus_dft.py
@@ -12,11 +12,25 @@ from reducible_colors import *
 class BeginIntroSampling_002(Scene):
     def construct(self):
         x_min = 0
-
         x_max = 2 * PI
+        frequency = 7
 
-        cosine_signal = get_cosine_func(freq=7)
+        cosine_signal = get_cosine_func(freq=frequency)
         axes, signal_mob = plot_time_domain(cosine_signal, t_max=x_max - PI / 4)
-        sampled_dots = get_sampled_dots(signal_mob, axes, x_max=x_max, num_points=7)
+        sampled_dots = get_sampled_dots(
+            signal_mob, axes, x_max=x_max, num_points=7
+        ).set_color(REDUCIBLE_VIOLET)
         self.play(Write(signal_mob))
         self.play(Write(sampled_dots))
+
+        double_sampling = get_sampled_dots(
+            signal_mob, axes, x_max=x_max, num_points=frequency * 2
+        )
+
+        self.wait()
+        self.play(Transform(sampled_dots, double_sampling[:-1], stretch=False))
+
+        shannon_sampling = get_sampled_dots(
+            signal_mob, axes, x_max=x_max, num_points=frequency * 2 + 1
+        )
+        self.play(Transform(sampled_dots, shannon_sampling[:-1], stretch=False))
