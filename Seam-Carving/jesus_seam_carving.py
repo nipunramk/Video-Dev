15778a65652b79164a9639907da5cea65b01586d
Jesús Rascón <blograso@gmail.com>
2023-03-02 20:19:09 +0100

    first version of recursion; not working yet

--- Seam-Carving/jesus_seam_carving.py
+++ Seam-Carving/jesus_seam_carving.py
@@ -51,10 +51,10 @@ class TestingSeams(Scene):
 
 class AllPossibleSeams(Scene):
     def construct(self):
-        pixel_array = np.random.randint(20, 200, (5, 5))
+        pixel_array = np.random.randint(20, 200, (3, 3))
         pix_arr_mob = PixelArray(img=pixel_array, color_mode="GRAY").scale(0.7)
 
-        self.generate_all_possible_seams(top_pixel=1, shape=(3, 3))
+        self.generate_all_seams_recursively(pixel_array, top_pixel=1)
 
     def generate_all_possible_seams(self, top_pixel=2, shape: tuple = (5, 5)):
         """
@@ -127,3 +127,32 @@ class AllPossibleSeams(Scene):
         # print(list(valid_combinations))
 
         return filtered_combinations
+
+    def generate_all_seams_recursively(self, pixel_array, top_pixel=2):
+        rows, cols = pixel_array.shape
+
+        def traverse(arr, seams: list, r, c):
+            # If the current position is the bottom-right corner of
+            # the matrix
+            if r <= rows - 1:
+                # Print the value at that position
+                seams.append(arr[r][c])
+                # End the recursion
+                return
+
+            # Print the value at the current position
+            seams.append(arr[r][c])
+            print(arr[r, c])
+
+            # If the end of the current row has not been reached
+            if r < rows:
+                traverse(arr, seams, r + 1, c)
+                if c > 0:
+                    traverse(arr, seams, r + 1, c - 1)
+                if c < cols:
+                    traverse(arr, seams, r + 1, c + 1)
+
+        seams = []
+        traverse(pixel_array, seams, 0, top_pixel)
+        print(seams)
+        return seams
